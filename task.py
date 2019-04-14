b2caf764c3e35a7e3cadeca8a37a68dc751e219e
l826568885v <48795251+l826568885v@users.noreply.github.com>
2019-04-14 20:00:52 +0800

    Add files via upload
    for task

---
+++ task.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+import os
+import sys
+import re
+repo1 = {repo_path:path1,branch:[branchA,branchB,branchC]}
+repo2 = {repo_path:path2,branch:[branchA,branchB,branchC]}
+repo3 = {repo_path:path3,branch:[branchA,branchB,branchC]}
+def get_repo(repo_path,path):
+    os.system("git clone repo_path path")
+if __name__ == "__main__":
+    print(os.path.abspath(os.path.dirname(__file__)))
+	
+	
+	
+	if file.endswith(".mak") or "makefile" in file: 
