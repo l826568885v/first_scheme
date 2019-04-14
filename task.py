de38bf7d6033b9fd3f3b69e7a914832c9bed8804
l826568885v <48795251+l826568885v@users.noreply.github.com>
2019-04-14 21:38:25 +0800

    Update task.py
    for updating code

--- task.py
+++ task.py
@@ -3,14 +3,44 @@
 import os
 import sys
 import re
-repo1 = {repo_path:path1,branch:[branchA,branchB,branchC]}
-repo2 = {repo_path:path2,branch:[branchA,branchB,branchC]}
-repo3 = {repo_path:path3,branch:[branchA,branchB,branchC]}
-def get_repo(repo_path,path):
-    os.system("git clone repo_path path")
+def create_repo(repo_num=3,branch_num=3):
+    for i in repo_num:
+        repo_name = "repo"+str(i)+"_info"
+        for num in branch_num:
+            repo_name["branches"]
+        repo_name = {"repo_add":"address1","repo_path":"path1","branches":['branchA','branchB','branchC']}
+repo1_info = {"repo_add":"address1","repo_path":"path1","branches":['branchA','branchB','branchC']}
+repo2_info = {"repo_add":"address2","repo_path":"path2","branches":['branchA','branchB','branchC']}
+repo3_info = {"repo_add":"address3","repo_path":"path3","branches":['branchA','branchB','branchC']}
+def get_repo(repo_path,path='.'):
+    if path is not '.':
+        os.system("git clone repo_path path")
+    else:
+        path = os.path.abspath(os.path.dirname(__file__))
+        os.system("git clone repo_path path")
+def get_log(repo_path,branch='master',num=10):
+    os.chdir(repo_path)
+    os.system('git checkout branch && git log --pretty=format:"%H %an %ae %ce %cd"')
+	
+	
+def revert_code(repo_add=".",branch_name="master",commit_ID):
+    if path is not '.':
+	    os.chdir(repo_add)
+        os.system("git checkout branch_name && git revert commit_ID && git push origin branch_name")
+    else: 
+        os.system("git checkout branch_name && git revert commit_ID && git push origin branch_name")
+def cherry_pick(repo_add=".",branch1,branch2,commit_ID):
+    if path is not '.':
+	    os.chdir(repo_add)
+		os.system("git checkout branch2 && git cherry-pick commit_ID && git push origin branch")
 if __name__ == "__main__":
     print(os.path.abspath(os.path.dirname(__file__)))
-	
-	
-	
-	if file.endswith(".mak") or "makefile" in file: 
+    num =3
+    print("repo"+str(num)+"_info")
+    print(chr(65))
+    flag = []
+    for i in 3:
+        flag[i] = chr(65+i)
+        print(flag[i])  
+    
+    # if file.endswith(".mak") or "makefile" in file: 
